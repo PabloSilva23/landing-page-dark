43f42db75e9c0326fac4f30585b1854585ac7b5e
PabloSilva23 <pablosilvax69@gmail.com>
2022-01-23 18:23:37 -0300

    troca de imagem

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const olhoSenha = document.querySelector('.olho-fechado');
+const olhoSenha = document.getElementById('olho-fechado');
 const senha = document.getElementById('password');
 olhoSenha.addEventListener('click', () => {
 
-    olhoSenha.classList.toggle('fa-smile-beam');
-    let inputsenha = senha.type == 'password'
+    let inputsenha = senha.type == 'password';
     if (inputsenha) {
+        olhoSenha.classList.toggle('fa-eye-slash');
         mostrarSenha()
     }
     else {
+        olhoSenha.classList.toggle('fa-eye-slash');
         ocultarSenha()
     }
 })
 
 function ocultarSenha() {
-    olhoSenha.setAttribute('src', '/img/eye-off.svg')
+    // olhoSenha.setAttribute('src', '/img/eye-off.svg')
     senha.setAttribute('type', 'password');
     
 }
 function mostrarSenha() {
-    olhoSenha.setAttribute('src', '/img/eye.svg')
+    // olhoSenha.setAttribute('src', '/img/eye.svg')
     senha.setAttribute('type', 'text');
 }
